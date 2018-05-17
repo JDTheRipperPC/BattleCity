745be90a3d670f1e0910d419a3443d282d2bf532
Pacho <Pacho@DESKTOP-Q6TF4Q2>
2018-05-17 19:48:10 +0200

    Basic squelet of the game

--- BattleCity/src/battlecity/game/Item.java
+++ BattleCity/src/battlecity/game/Item.java
@@ -1,9 +1,128 @@
 package battlecity.game;
 
+import battlecity.socket.ClientSocket;
+import java.awt.image.BufferedImage;
+
 /**
  *
  * @author xGod
  */
-public abstract class Item implements Runnable{
+public abstract class Item implements Runnable {
+
+    private int axisX;
+    private int axisY;
+    private int life;
+    private float speed;
     
+    private BufferedImage imagenPath;
+
+    public Item(int axisX, int axisY, int life, float speed, BufferedImage imagenPath) {
+        this.axisX = axisX;
+        this.axisY = axisY;
+        this.life = life;
+        this.speed = speed;
+        this.imagenPath = imagenPath;
+    }
+    
+    
+
+    /**
+     * Orientation class to help us rotate objects and know their direction
+     */
+    public static enum Orientation {
+
+        NORTH(0, -1, 0), SOUTH(0, 1, 180), WEST(-1, 0, 270), EAST(1, 0, 90);
+        private int axisX;
+        private int axisY;
+        private int degrees;
+
+        /**
+         * Creates orientation.
+         *
+         *
+         * @param axisX
+         * @param axisY
+         * @param degrees
+         */
+        private Orientation(int axisX, int axisY, int degrees) {
+            this.axisX = axisX;
+            this.axisY = axisY;
+            this.degrees = degrees;
+        }
+
+        public int getAxisX() {
+            return axisX;
+        }
+
+        public void setAxisX(int axisX) {
+            this.axisX = axisX;
+        }
+
+        public int getAxisY() {
+            return axisY;
+        }
+
+        public void setAxisY(int axisY) {
+            this.axisY = axisY;
+        }
+
+        public int getDegrees() {
+            return degrees;
+        }
+
+        public void setDegrees(int degrees) {
+            this.degrees = degrees;
+        }
+    }
+
+
+    //---------------------- GETTERS AND SETTERS -------------------------------
+
+    public int getAxisX() {
+        return axisX;
+    }
+
+    public int getAxisY() {
+        return axisY;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public BufferedImage getImagenPath() {
+        return imagenPath;
+    }
+
+
+    public void setAxisX(int axisX) {
+        this.axisX = axisX;
+    }
+
+    public void setAxisY(int axisY) {
+        this.axisY = axisY;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public void setImagenPath(BufferedImage imagenPath) {
+        this.imagenPath = imagenPath;
+    }
+
+    
+    //---------------------- Publics -------------------------------------------    
+
+
+    
+
 }
--- BattleCity/src/battlecity/game/items/Tank.java
+++ BattleCity/src/battlecity/game/items/Tank.java
@@ -1,6 +1,8 @@
 package battlecity.game.items;
 
 import battlecity.game.Item;
+import battlecity.socket.ClientSocket;
+import java.awt.image.BufferedImage;
 
 /**
  *
@@ -8,9 +10,18 @@ import battlecity.game.Item;
  */
 public class Tank extends Item {
 
+    private ClientSocket cs;
+
+    public Tank(int axisX, int axisY, int life, float speed, BufferedImage imagenPath, ClientSocket cs) {
+        super(axisX, axisY, life, speed, imagenPath);
+        this.cs = cs;
+    }
+
+    
+   
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
-    
+
 }
