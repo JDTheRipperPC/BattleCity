21c7661728395e24a39ddcdfd9c9725f5cc09d91
JDTheRipperPC <jdtheripperpc@gmail.com>
2018-05-22 00:55:17 +0200

    Sockets update

--- BattleCity/src/battlecity/gui/Viewer.java
+++ BattleCity/src/battlecity/gui/Viewer.java
@@ -1,8 +1,10 @@
 package battlecity.gui;
 
+import battlecity.socket.ClientSocket;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.util.ArrayList;
 
 /**
  *
@@ -13,6 +15,8 @@ public class Viewer extends Canvas implements Runnable{
      private Dimension dim;
      private Scene sc;
 
+     private ArrayList<ClientSocket> clients;
+     
     public Viewer(Dimension dim) {
         this.dim = dim;
         initProperties();
@@ -29,6 +33,15 @@ public class Viewer extends Canvas implements Runnable{
     
     private void initObjects(){
         sc = new Scene();
+        clients = new ArrayList<>();
+    }
+
+    public ArrayList<ClientSocket> getClients() {
+        return clients;
+    }
+
+    public void setClients(ArrayList<ClientSocket> clients) {
+        this.clients = clients;
     }
 
     public Scene getSc() {
